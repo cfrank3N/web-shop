b7e824bab53b0da04851db08e95e5377eb6d8106
ArvidUtas <arvidu@hotmail.com>
2025-03-26 20:40:08 +0100

    added js method to replace main, not currently working

--- main.js
+++ main.js
@@ -17,11 +17,45 @@ async function main() {
     document.getElementById(`title${i}`).innerHTML = products[i].title;
     document.getElementById(`desc${i}`).innerHTML = products[i].description; 
   }
-
 }
+main(); 
 
-main();
+/* Den här metoden ersätter main(). Då skulle man inte behöva ha några kort i index. Funkar inte just nu
+async function populateProducts() {
+  await fetchProducts();
+  let output = `<div class="row">`;
+  for (let i in products){
+    if (i%4==0)
+      output += `</div><div class="row">`;
 
+    output += `
+        <!-- cards: -->
+        <div class="col-sm-6 col-lg-3 my-3">
+            <div class="card shadow h-100 shadow scale-on-hover cursor-pointer" role="button"">
+                <div class="card-body data-bs-toggle="modal" data-bs-target="#productModal" onclick="populateProductPopUp(${i})">
+                    <!-- pics -->
+                    <div class="productImage">
+                        <img src="${products[i].image}" style="max-height: 15rem"/>
+                    </div>
+                    <!-- info -->
+                    <div class="product-text-container">
+                        <h5 class="product-title mb-1">${products[i].title}</h5>
+                        <p class="card-text product-desc mb-0">${products[i].description}</p>
+                    </div>
+                </div>
+                <!-- bottom section -->
+                <div class="price-buy-container">
+                    <span class="price-text">${products[i].price}</span>
+                    <div class="btn btn-custom">buy</div>
+                </div>
+            </div>
+        </div>`
+  }
+  output += `</div>`;
+  document.getElementById('prod-container').innerHTML = output;
+}
+populateProducts();
+*/
 
 function popupFn() {
   document.getElementById(
