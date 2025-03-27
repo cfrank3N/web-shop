492c3f1c21b70e20d4ae98a0971235cbe5cdc54d
Adam Frank <adamcecarfrank@gmail.com>
2025-03-27 15:15:29 +0100

    Added validation script in main.js And some style in form.html. Not done

--- main.js
+++ main.js
@@ -1,3 +1,60 @@
+//Validation for the form
+  //Valid expressions for each field
+  const regexName = new RegExp(/^(?!.{50})[A-Za-zåäöÅÄÖ\s]+\s[A-Za-zåäöÅÄÖ\s]+$/); //Done
+  const regexPhone = new RegExp(/^[0-9\-\(\)\s]{4,50}$/); //Done
+  const regexEmail = new RegExp(/^(?!.{50})[A-Za-zåäöÅÄÖ\.-_0-9]+@[A_Za-zåäöÅÄÖ\.-_0-9]+\.[a-z]{2,}$/); //Done
+  const regexStreet = new RegExp(/^(?!.{50})[A-Za-zåäöÅÄÖ\s0-9]{2,}$/); //Done
+  const regexZipCode = new RegExp(/^[0-9]{5}|[0-9]{3}\s[0-9]{2}$/); //Done
+  const regexCity = new RegExp(/^(?!.{50})[A-Za-zåäöÅÄÖ\s]{2,}$/); //Done
+
+  const validations = [regexName, regexPhone, regexEmail, regexStreet, regexZipCode, regexCity];
+
+//Adds eventlistener to form to perform validation
+document.getElementById("myForm").addEventListener("submit", validateFields);
+
+//Validation function for the form
+function validateFields(e) {
+  //Prevents the form from submitting and refreshing
+  e.preventDefault();
+  
+  //All input values in the form to preform validation on
+  let fullName = document.getElementById("name");
+  let phone = document.getElementById("phone");
+  let email = document.getElementById("email");
+  let street = document.getElementById("street");
+  let zipCode = document.getElementById("zipCode");
+  let city = document.getElementById("city");
+  
+  //Adds the input values to an array to validate everything in a for loop
+  const values = [fullName, phone, email, street, zipCode, city];
+
+  //Validating each field against it's corresponding expression
+  for (i in values) {
+
+    //If the expression is true add "is-valid" to the values class to display a green check-icon"
+    if (validations[i].test(values[i].value)) {
+      
+      //Removes "is-invalid" if it was invalid in the previous attempt
+      if (values[i].classList.contains("is-invalid")) {
+        values[i].classList.remove("is-invalid");
+      }
+
+      //Adds valid to the input
+      values[i].classList.add("is-valid");
+
+    } else {
+      //Removes "is-valid" if it was valid in the previous attempt
+      if (values[i].classList.contains("is-valid")) {
+        values[i].classList.remove("is-valid");
+      }
+
+      //Adds invalid to the input
+      values[i].classList.add("is-invalid");
+    }
+  }
+}
+//End of validation script
+
 let products = [];
 
 async function fetchProducts() {
@@ -20,7 +77,7 @@ async function main() {
 
 }
 
-main();
+//main();
 
 
 function popupFn() {
