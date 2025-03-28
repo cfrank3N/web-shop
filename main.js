8cabc90352b1583082eba54861d45995950108cc
ArvidUtas <arvidu@hotmail.com>
2025-03-28 12:19:33 +0100

    removed description from cards

--- main.js
+++ main.js
@@ -22,18 +22,17 @@ async function populateProducts() {
             <div class="card h-100 shadow scale-on-hover cursor-pointer rounded-5" role="button"">
                 <div class="card-body" data-bs-toggle="modal" data-bs-target="#productModal" onclick="populateProductPopUp(${i})">
                     <!-- pics -->
-                    <div class="position-relative" style="height: 15rem">
+                    <div class="position-relative mt-3" style="height: 15rem">
                         <img src="${products[i].image}" class="card-img-top position-absolute top-50 start-50 translate-middle img-fluid w-100 object-fit-contain" style="max-height: 15rem"/>
                     </div>
                     <!-- info -->
-                    <div class="product-text-container">
-                        <h5 class="product-title mb-1">${getFirstFiveWords(products[i].title)}</h5>
-                        <p class="card-text product-desc mb-0">${products[i].description}</p>
+                    <div class="mt-4 ms-2">
+                        <h5 class="product-title">${getFirstFiveWords(products[i].title)}</h5>
                     </div>
                 </div>
                 <!-- bottom section -->
-                <div class="price-buy-container">
-                    <span class="price-text">€${products[i].price.toFixed(2)}</span>
+                <div class="d-flex justify-content-between align-items-center mx-4 mb-4">
+                    <span class="fw-bold">€${products[i].price.toFixed(2)}</span>
                     <div class="btn btn-custom">Buy</div>
                 </div>
             </div>
